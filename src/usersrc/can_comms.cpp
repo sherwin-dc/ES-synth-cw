696050caf0c6917a8c97a6f71ce7bad999c7f4f3
tianyilim <0.tianyi.lim@gmail.com>
2022-03-15 16:38:42 +0000

    Add debug hooks to CAN bus irqs

--- src/usersrc/can_comms.cpp
+++ src/usersrc/can_comms.cpp
@@ -5,6 +5,7 @@
 #include "task.h"
 #include "keymat.h"
 #include "can_comms.h"
+#include "can.h"
 #include "main.h"
 
 // for debug only
@@ -16,44 +17,30 @@
 #include <algorithm> // Contains max and min functions
 
 //Overwrite the weak default IRQ Handlers and callabcks
-extern "C" void CAN1_RX0_IRQHandler(void);
-extern "C" void CAN1_TX_IRQHandler(void);
+// extern "C" void CAN1_RX0_IRQHandler(void);
+// extern "C" void CAN1_TX_IRQHandler(void);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 // Must match the declaration in the header file
 #ifdef __cplusplus
     //Pointer to user ISRS
-    extern "C" void (*CAN_RX_ISR_PTR)() = NULL;
-    extern "C" void (*CAN_TX_ISR_PTR)() = NULL;
+    // extern "C" void (*CAN_RX_ISR_PTR)() = NULL;
+    // extern "C" void (*CAN_TX_ISR_PTR)() = NULL;
+    void (*CAN_RX_ISR_PTR)() = NULL;
+    void (*CAN_TX_ISR_PTR)() = NULL;
+
 #else
     void (*CAN_RX_ISR_PTR)() = NULL;
     void (*CAN_TX_ISR_PTR)() = NULL;
 #endif
 
-
-//CAN handle struct with initialisation parameters
-//Timing from http://www.bittiming.can-wiki.info/ with bit rate = 125kHz and clock frequency = 80MHz
-CAN_HandleTypeDef CAN_Handle = {
-    CAN1,
-    {
-        40,           //Prescaler
-        CAN_MODE_NORMAL,     //Normal/loopback/silent mode
-        CAN_SJW_2TQ,  //SyncJumpWidth
-        CAN_BS1_13TQ, //TimeSeg1
-        CAN_BS2_2TQ,  //TimeSeg2
-        DISABLE,      //TimeTriggeredMode
-        DISABLE,      //AutoBusOff
-        ENABLE,       //AutoWakeUp
-        ENABLE,       //AutoRetransmission
-        DISABLE,      //ReceiveFifoLocked
-        ENABLE        //TransmitFifoPriority
-    },
-    HAL_CAN_STATE_RESET,  //State
-    HAL_CAN_ERROR_NONE    //Error Code
-};
-
 /*
 //Initialise CAN dependencies: GPIO and clock
-void HAL_CAN_MspInit(CAN_HandleTypeDef* CAN_Handle) {
+void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan1) {
 
   //Set up the pin initialisation
   GPIO_InitTypeDef GPIO_InitCAN_TX = {
@@ -84,8 +71,8 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* CAN_Handle) {
 
 uint32_t CAN_Init(bool loopback) {
   if (loopback)
-    CAN_Handle.Init.Mode = CAN_MODE_LOOPBACK;
-  return (uint32_t) HAL_CAN_Init(&CAN_Handle);
+    hcan1.Init.Mode = CAN_MODE_LOOPBACK;
+  return (uint32_t) HAL_CAN_Init(&hcan1);
 }
 
 
@@ -105,12 +92,12 @@ uint32_t setCANFilter(uint32_t filterID, uint32_t maskID, uint32_t filterBank) {
     0                           //uint32_t SlaveStartFilterBank
   };
 
-  return (uint32_t) HAL_CAN_ConfigFilter(&CAN_Handle, &filterInfo);
+  return (uint32_t) HAL_CAN_ConfigFilter(&hcan1, &filterInfo);
 }
 
 
 uint32_t CAN_Start() {
-  return (uint32_t) HAL_CAN_Start(&CAN_Handle);
+  return (uint32_t) HAL_CAN_Start(&hcan1);
 }
 
 
@@ -127,15 +114,15 @@ uint32_t CAN_TX(uint32_t ID, uint8_t data[8]) {
   };
 
   //Wait for free mailbox
-  while (!HAL_CAN_GetTxMailboxesFreeLevel(&CAN_Handle));
+  while (!HAL_CAN_GetTxMailboxesFreeLevel(&hcan1));
 
   //Start the transmission
-  return (uint32_t) HAL_CAN_AddTxMessage(&CAN_Handle, &txHeader, data, NULL);
+  return (uint32_t) HAL_CAN_AddTxMessage(&hcan1, &txHeader, data, NULL);
 }
 
 
 uint32_t CAN_CheckRXLevel() {
-  return HAL_CAN_GetRxFifoFillLevel(&CAN_Handle, 0);
+  return HAL_CAN_GetRxFifoFillLevel(&hcan1, 0);
 }
 
 
@@ -143,10 +130,10 @@ uint32_t CAN_RX(uint32_t *ID, uint8_t data[8]) {
   CAN_RxHeaderTypeDef rxHeader;
 
   //Wait for message in FIFO
-  while (!HAL_CAN_GetRxFifoFillLevel(&CAN_Handle, 0));
+  while (!HAL_CAN_GetRxFifoFillLevel(&hcan1, 0));
   
   //Get the message from the FIFO
-  uint32_t result = (uint32_t) HAL_CAN_GetRxMessage(&CAN_Handle, 0, &rxHeader, data);
+  uint32_t result = (uint32_t) HAL_CAN_GetRxMessage(&hcan1, 0, &rxHeader, data);
 
   //Store the ID from the header
   *ID = rxHeader.StdId;
@@ -159,113 +146,100 @@ uint32_t CAN_RegisterRX_ISR(void(*callback)()) {
   CAN_RX_ISR_PTR = callback;
 
   //Enable message received interrupt in HAL
-  uint32_t status = (uint32_t) HAL_CAN_ActivateNotification (&CAN_Handle, CAN_IT_RX_FIFO0_MSG_PENDING);
+  uint32_t status = (uint32_t) HAL_CAN_ActivateNotification (&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 
   //Switch on the interrupt
-  HAL_NVIC_SetPriority (CAN1_RX0_IRQn, 6, 0);
-  HAL_NVIC_EnableIRQ (CAN1_RX0_IRQn);
+//   HAL_NVIC_SetPriority (CAN1_RX0_IRQn, 5, 0);
+//   HAL_NVIC_EnableIRQ (CAN1_RX0_IRQn);
 
   return status;
 }
 
-/*
-uint32_t CAN_RegisterTX_ISR(void(& callback)()) {
+uint32_t CAN_RegisterTX_ISR(void(*callback)()) {
   //Store pointer to user ISR
-  CAN_TX_ISR_PTR = &callback;
+  CAN_TX_ISR_PTR = callback;
 
   //Enable message received interrupt in HAL
-  uint32_t status = (uint32_t) HAL_CAN_ActivateNotification (&CAN_Handle, CAN_IT_TX_MAILBOX_EMPTY);
+  uint32_t status = (uint32_t) HAL_CAN_ActivateNotification (&hcan1, CAN_IT_TX_MAILBOX_EMPTY);
 
   //Switch on the interrupt
-  HAL_NVIC_SetPriority (CAN1_TX_IRQn, 6, 0);
-  HAL_NVIC_EnableIRQ (CAN1_TX_IRQn);
+//   HAL_NVIC_SetPriority (CAN1_TX_IRQn, 5, 0);
+//   HAL_NVIC_EnableIRQ (CAN1_TX_IRQn);
 
   return status;
 }
-*/
 
 void HAL_CAN_RxFifo0MsgPendingCallback (CAN_HandleTypeDef * hcan){
-
-  //Call the user ISR if it has been registered
-  if (CAN_RX_ISR_PTR)
-    CAN_RX_ISR_PTR();
+    DEBUG_PRINT("CAN_RX_IRQ");
+    // Call the user ISR if it has been registered
+    if (CAN_RX_ISR_PTR)
+        CAN_RX_ISR_PTR();
 }
-
 
 void HAL_CAN_TxMailbox0CompleteCallback (CAN_HandleTypeDef * hcan){
-
-  //Call the user ISR if it has been registered
-  if (CAN_TX_ISR_PTR)
-    CAN_TX_ISR_PTR();
+    DEBUG_PRINT("CAN_TX_IRQ");
+    //Call the user ISR if it has been registered
+    if (CAN_TX_ISR_PTR)
+        CAN_TX_ISR_PTR();
 }
-
 
 void HAL_CAN_TxMailbox1CompleteCallback (CAN_HandleTypeDef * hcan){
-
-  //Call the user ISR if it has been registered
-  if (CAN_TX_ISR_PTR)
-    CAN_TX_ISR_PTR();
+    DEBUG_PRINT("CAN_TX_IRQ");
+    //Call the user ISR if it has been registered
+    if (CAN_TX_ISR_PTR)
+        CAN_TX_ISR_PTR();
 }
-
 
 void HAL_CAN_TxMailbox2CompleteCallback (CAN_HandleTypeDef * hcan){
-
-  //Call the user ISR if it has been registered
-  if (CAN_TX_ISR_PTR)
-    CAN_TX_ISR_PTR();
+    DEBUG_PRINT("CAN_TX_IRQ");
+    //Call the user ISR if it has been registered
+    if (CAN_TX_ISR_PTR)
+        CAN_TX_ISR_PTR();
 }
-
-
-//This is the base ISR at the interrupt vector
-void CAN1_RX0_IRQHandler(void){
-
-  //Use the HAL interrupt handler
-  HAL_CAN_IRQHandler(&CAN_Handle);
-}
-
-
-//This is the base ISR at the interrupt vector
-void CAN1_TX_IRQHandler(void){
-
-  //Use the HAL interrupt handler
-  HAL_CAN_IRQHandler(&CAN_Handle);
-}
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void CAN_RX_ISR() {
     uint8_t RX_Message_ISR[8];
     uint32_t ID;
-    CAN_RX(&ID, RX_Message_ISR);
-    xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+    uint32_t rx_res = CAN_RX(&ID, RX_Message_ISR);
+    BaseType_t queueSendRes = pdTRUE; // Placeholder
+    // ? xQueueSendFromISR hangs the program for some reason
+    // queueSendRes = xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+    print(queueSendRes);
 }
 
-#ifdef __cplusplus
-}
-#endif
 
 // Create a decode thread to handle incoming CAN messages
 void decodeCANMessages(void *params) {
     // DEBUG_PRINT("~");
     while(1){
-        // DEBUG_PRINT("!");
-        // if ( xQueueReceive(msgInQ, RX_Message, portMAX_DELAY)  == pdPASS ){
-        if ( xQueueReceive(msgInQ, RX_Message, 0)  == pdPASS ){
-            DEBUG_PRINT("can rx smth");
-        } else {
-            // DEBUG_PRINT("x");
-        }
+      DEBUG_PRINT("!");
+      BaseType_t queueReceiveRes = pdPASS;  // Placeholder
+      // ? xQueueReceive also blocks the program.
+      // ? Try toggling between portMAX_DELAY (blocks forever) and (TickType_t)0 (no blocking) for testing
+      // queueReceiveRes = xQueueReceive(msgInQ, RX_Message, (TickType_t)0);
+      if ( queueReceiveRes == pdPASS ){
+          DEBUG_PRINT("can rx smth");
+          // TODO decode thread in response to incoming messages
+
+      } else {
+          // DEBUG_PRINT("x");
+      }
     }
+
+    DEBUG_PRINT("decodeCANMessages exited!");
+    vTaskDelete(NULL);
 
 }
 
 void init_can_rx_decode(){
     DEBUG_PRINT("Initializing CAN RX Decode");
-    xQueueReset(msgInQ);
-    if (xTaskCreate(decodeCANMessages, "CAN RX ISR", 64, NULL, 1, NULL) != pdPASS) {
+    xQueueReset(msgInQ);    // Ensure message queue is empty
+    if (xTaskCreate(decodeCANMessages, "CAN RX ISR", 128, NULL, 1, NULL) != pdPASS) {
         DEBUG_PRINT("Error. Free memory: ");
         print(xPortGetFreeHeapSize());
     }
 }
+
+#ifdef __cplusplus
+}
+#endif
