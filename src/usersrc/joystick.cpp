9c301355e1b34bc85183d9fdefeecedc43134531
sherwin-dc <59867245+sherwin-dc@users.noreply.github.com>
2022-03-18 23:43:26 +0000

    Read joystick with DMA

--- src/usersrc/joystick.cpp
+++ src/usersrc/joystick.cpp
@@ -16,6 +16,7 @@ const uint32_t x_mid = 1790;
 const uint32_t xy_min = 0;
 const uint32_t xy_max = 4030;
 
+uint32_t ADC[2];
 
 //JOYSTICK SHOULD BE USED TO MODULATE THE FREQUENCY 
 void readJoystick(void* params) {
@@ -26,9 +27,9 @@ void readJoystick(void* params) {
         DEBUG_PRINT("1");
         // delay_microseconds(500);
 
-        HAL_ADC_Start(&hadc1);
-        HAL_ADC_PollForConversion(&hadc1, joystickMaxDelay);
-        uint32_t rawx = HAL_ADC_GetValue(&hadc1);
+        // HAL_ADC_Start(&hadc1);
+        // HAL_ADC_PollForConversion(&hadc1, joystickMaxDelay);
+        // uint32_t rawx = HAL_ADC_GetValue(&hadc1);
 
         // delay_microseconds(5);
         // for(int i=0; i<100; i++) {
@@ -37,27 +38,29 @@ void readJoystick(void* params) {
         // };
         DEBUG_PRINT("2");
 
-        HAL_ADC_PollForConversion(&hadc1, joystickMaxDelay);
-        uint32_t rawy = HAL_ADC_GetValue(&hadc1);
+        // HAL_ADC_PollForConversion(&hadc1, joystickMaxDelay);
+        // uint32_t rawy = HAL_ADC_GetValue(&hadc1);
         
 
-        HAL_ADC_Stop(&hadc1);
-
-        print(rawx);
-        print(rawy);
+        // HAL_ADC_Stop(&hadc1);
+        uint32_t v1, v2;
+        v1 = __atomic_load_n(&ADC[0], __ATOMIC_RELAXED);
+        v2 = __atomic_load_n(&ADC[1], __ATOMIC_RELAXED);
+        print(v1);
+        print(v2);
         // delay_microseconds(500);
 
 
         // normalise the values
-        int8_t y = normaliseJoystick(rawy, xy_min, y_mid, xy_max);
-        int8_t x = normaliseJoystick(rawx, xy_min, x_mid, xy_max);
+        // int8_t y = normaliseJoystick(rawy, xy_min, y_mid, xy_max);
+        // int8_t x = normaliseJoystick(rawx, xy_min, x_mid, xy_max);
 
-        // joystick values are absolute (no need to depend on prev values)
-        __atomic_store_n(&pitch,y,__ATOMIC_RELAXED);
-        __atomic_store_n(&modulation,x,__ATOMIC_RELAXED);
+        // // joystick values are absolute (no need to depend on prev values)
+        // __atomic_store_n(&pitch,y,__ATOMIC_RELAXED);
+        // __atomic_store_n(&modulation,x,__ATOMIC_RELAXED);
 
         // Toggle MCU LED
-        HAL_GPIO_TogglePin(GPIOB, LED_BUILTIN_Pin);
+        // HAL_GPIO_TogglePin(GPIOB, LED_BUILTIN_Pin);
         // DEBUG_PRINT("3");
 
         // vTaskDelay( pdMS_TO_TICKS(100) );
@@ -67,7 +70,10 @@ void readJoystick(void* params) {
 
 void init_joystick() {
     DEBUG_PRINT("Initializing Joystick Read");
-    if (xTaskCreate(readJoystick, "Read Joystick", 128, NULL, 2, NULL) != pdPASS) {
+    HAL_ADC_Start_DMA(&hadc1, ADC, 2);
+
+
+    if (xTaskCreate(readJoystick, "Read Joystick", 256, NULL, 2, NULL) != pdPASS) {
         DEBUG_PRINT("ERROR");
         print(xPortGetFreeHeapSize());
     }
